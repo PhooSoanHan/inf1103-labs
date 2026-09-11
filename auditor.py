9ce84714633209c5eb314181a241d512c65b2955
PhooSoanHan <phoosoanhan61@gmail.com>
2026-09-11 11:00:58 +0800

    Requirement 8

--- auditor.py
+++ auditor.py
@@ -1,4 +1,5 @@
 inventory = 0
+failed_entries = 0
 
 while True:
     stock = input("Enter the stock quantity: ")
@@ -8,10 +9,12 @@ while True:
 
     if stock.startswith("-") and stock[1:].isdigit():
         print("Please enter the valid number.")
+        failed_entries += 1
         continue
 
     if not stock.isdigit():
         print("Please enter the valid integer.")
+        failed_entries += 1
         continue
 
     stock = int(stock)
@@ -22,3 +25,7 @@ while True:
     if inventory > 500:
         print("The total inventory cannot exceed 500 units!")
         break
+
+print("=========================")
+print("Total Units Processed: ", inventory)
+print("Number of failed entries: ", failed_entries)
